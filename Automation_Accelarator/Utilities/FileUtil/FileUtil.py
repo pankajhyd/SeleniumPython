11339527d5b6a25527f7855d013e3509a9f6358b
admin <mailtopankaj1981@gmail.com>
2018-09-11 17:33:31 +0530

    File Structure Ver 1.0

--- Automation_Accelarator/Utilities/FileUtil/FileUtil.py
+++ Automation_Accelarator/Utilities/FileUtil/FileUtil.py
@@ -8,3 +8,71 @@ class FileUtil:
     '''
     This Class is used for File and Folder related operation
     '''
+    @staticmethod
+    def fnCreateFolder(self,folderPath,folderName):
+        '''
+        Function Description : This Function is used to create folder
+        :param folderPath: Folder Path
+        :param folderName: Folder Name
+        :return:
+        '''
+    @staticmethod
+    def fnRenameFolder(self,folderPath,folderName,newFolderName):
+        '''
+        Function Description : This Function is used to Rename folder
+        :param folderPath: Folder Path
+        :param folderName: Folder Name
+        :param newFolderName : New Name of Folder
+        :return:
+        '''
+    @staticmethod
+    def fnDeleteFolder(self,folderPath,folderName):
+      '''
+      Function Description : This Function is used to Delete folder
+      :param folderPath: Foder Path
+      :param folderName: Folder Name
+      :return:
+      '''
+    @staticmethod
+    def fnSearchFolder(self,folderPath,folderName):
+      '''
+      Function Description : This Function is used to Delete folder
+      :param folderPath: Folder Path
+      :param folderName: Folder Name
+      :return:
+      '''
+    @staticmethod
+    def fnCreateFile(self, folderPath, fileName):
+        '''
+        Function Description : This Function is used to create folder
+        :param folderPath: Folder Path
+        :param fileName: File Name
+        :return:
+        '''
+    @staticmethod
+    def fnRenameFile(self, folderPath, fileName, newFileName):
+        '''
+        Function Description : This Function is used to Rename folder
+        :param folderPath: Folder Path
+        :param fileName: File Name
+        :param newFileName : New Name of File
+        :return:
+        '''
+    @staticmethod
+    def fnDeleteFile(self, folderPath, fileName):
+        '''
+        Function Description : This Function is used to Delete folder
+        :param folderPath: Folder Path
+        :param fileName: File Name
+        :return:
+        '''
+    @staticmethod
+    def fnSearchFile(self, folderPath, fileName):
+        '''
+        Function Description : This Function is used to Delete folder
+        :param folderPath: Folder Path
+        :param fileName: File Name
+        :return:
+        '''
+
+print(FileUtil.fnCreateFile.__doc__)
